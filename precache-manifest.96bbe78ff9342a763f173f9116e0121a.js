53386cc002452ad41f13f713f7499738c9dbda27
Logan-Code-LV <logangayler@gmail.com>
2020-05-14 12:10:29 -0700

    Updates

---
+++ precache-manifest.96bbe78ff9342a763f173f9116e0121a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6c9e011b062e69692305908ddc42ef",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "d5fbdcbb2ab40716179a",
+    "url": "/react-to-do-list/static/css/main.10cd74db.chunk.css"
+  },
+  {
+    "revision": "991983fe41a9240c84d9",
+    "url": "/react-to-do-list/static/js/2.85b9f8a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-to-do-list/static/js/2.85b9f8a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5fbdcbb2ab40716179a",
+    "url": "/react-to-do-list/static/js/main.99386530.chunk.js"
+  },
+  {
+    "revision": "e04cdc0a925ade549f12",
+    "url": "/react-to-do-list/static/js/runtime-main.1ae83713.js"
+  }
+]);
